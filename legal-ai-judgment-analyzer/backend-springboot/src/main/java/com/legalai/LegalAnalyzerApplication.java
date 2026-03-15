582b13acd557d75431ec75169099c779202cdf6b
SaiSampreethiGourishetty <65446432+SaiSampreethiGourishetty@users.noreply.github.com>
2026-03-15 23:09:17 +0530

    Create LegalAnalyzerApplication.java

---
+++ legal-ai-judgment-analyzer/backend-springboot/src/main/java/com/legalai/LegalAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.legalai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegalAnalyzerApplication.class, args);
+    }
+
+}
